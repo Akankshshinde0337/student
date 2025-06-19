1622c247ba0077301a15b711b2e716e876a6bdae
Jvivek133 <vivekjadhav1255@gmail.com>
2025-06-19 13:22:34 +0530

    repo

---
+++ src/main/java/com/example/student/Controller.java
@@ -0,0 +1,16 @@
+package com.example.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/getStudentInfo/{id}")
+	public String getStudentinfo(int id) {
+		
+
+		return "akanksha shinde";
+	}
+	
+}
